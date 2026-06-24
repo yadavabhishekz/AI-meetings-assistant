b3dc75afaee6c689b3b6f6a6d6fdf433240bcc6a
yadavabhishekz <yadavabhishekz1347@gmail.com>
2026-06-24 23:58:51 +0530

    yt audio downloader working

--- utils/audio_processor.py
+++ utils/audio_processor.py
@@ -2,8 +2,8 @@ import yt_dlp
 from pydub import  AudioSegment
 import os
 
-DOWNLAOD_DIR  = 'downloads'
-os.makedirs(DOWNLAOD_DIR, exist_ok=True)
+DOWNLOAD_DIR  = 'downloads'
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
 
 def download_youtube_audio(url :str) ->str:
     output_path = os.path.join(DOWNLOAD_DIR, "%(title)s.%(ext)s")
